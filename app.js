3ee2576544444d68bf1259261a26a4d304e25836
Abenezer <monjo003@morris.umn.edu>
2020-04-16 22:03:03 -0500

    using node to run the app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+    res.render(__dirname+'/views/index');
+});
+
+
+app.listen(4000, function() {
+  console.log("Server stated on port 4000");
+});
